e83389af69d79c8003421c8c029aa0b14337903d
abidkhan04 <khanabid1978004@gmail.com>
2025-03-06 05:34:55 +0500

    Form Added in the Footer and Sales component

--- components/pages/home/Footer.vue
+++ components/pages/home/Footer.vue
@@ -1,12 +1,17 @@
 <template>
+  <div class="block md:hidden">
+    <Form />
+  </div>
   <div class="bg-black py-8 pb-24 sm:pb-8 md:py-6 2xl:py-12">
     <BaseContainer>
-      <div class="flex flex-col md:flex-row items-center justify-between gap-10 text-white">
+      <div
+        class="flex flex-col md:flex-row items-center justify-between gap-10 text-white"
+      >
         <div class="flex flex-col items-center md:items-start gap-4 md:gap-3">
           <img src="/images/logo.png" class="w-[165px]" />
           <p class="md:text-lg 2xl:xl font-bold">Where dreams come home.</p>
         </div>
-        <div class=" flex flex-col items-center md:items-end gap-6 md:gap-3">
+        <div class="flex flex-col items-center md:items-end gap-6 md:gap-3">
           <div class="flex items-center gap-3 order-1 md:order-none">
             <div class="bg-white rounded-full p-2">
               <IconFacebook />
@@ -18,7 +23,7 @@
               <IconInstagram />
             </div>
           </div>
-          <div class=" text-center text-xs w-[80%] md:w-full">
+          <div class="text-center text-xs w-[80%] md:w-full">
             &#169; 2024 Dubai Property Offer All Rights Reserved
           </div>
         </div>
--- components/pages/home/Sales.vue
+++ components/pages/home/Sales.vue
@@ -3,9 +3,9 @@
     <div
       class="bg-[#1EBEF1] text-white text-2xl lg:text-4xl 2xl:text-6xl text-center font-semibold px-2 py-8 relative"
     >
-    <BaseContainer>
+      <BaseContainer>
       <h3 class="text-xl md:text-3xl">Invest in RERA-Certified Project with a developer renowned for delivering on-time</h3>
-    </BaseContainer>
+      </BaseContainer>
     </div>
     <BaseContainer>
       <div class="grid grid-cols-2 gap-y-4 xs:gap-6 pt-12 lg:pt-8 pb-8">
@@ -17,6 +17,9 @@
           <h3 class="text-lg xs:text-2xl sm:text-3xl lg:text-4xl 2xl:text-6xl font-semibold">Exclusive</h3>
           <p class="text-sm xs:text-base 2xl:text-xl font-medium">Payment Plans</p>
         </div>
+      </div>
+      <div class="block md:hidden">
+        <Form />
       </div>
     </BaseContainer>
   </div>
